2f1372ce42aa3e2781895f2483091c66fa3508be
Dmytro20777 <08dmutro11@gmail.com>
2024-03-17 12:13:47 +0200

    Task2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { Header } from 'components';
-import { lazy } from 'react';
+import { lazy, useEffect } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
+import { getUserInfo } from './service';
 
 const HomePage = lazy(() => import('./pages/Home'));
 const RatesPage = lazy(() => import('./pages/Rates'));
 
 export const App = () => {
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(position => {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      getUserInfo({latitude, longitude })
+    })
+  }, [])
   return (
     <Routes>
       <Route path="/" element={<Header />}>
