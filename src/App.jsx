38afa0d9da7d560620f96bafed6a6d66b43d9bae
Valentyn Savytskyi <valentynsavitskyy@gmail.com>
2024-03-17 10:05:34 +0100

    create routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import { Heading } from 'components';
+import { Header } from 'components';
+import { lazy } from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+
+const HomePage = lazy(() => import('./pages/Home'));
+const RatesPage = lazy(() => import('./pages/Rates'));
 
 export const App = () => {
   return (
-    <div>
-      <Heading title="Just do it!" />
-      <p>Hello</p>
-    </div>
+    <Routes>
+      <Route path="/" element={<Header />}>
+        <Route index element={<HomePage />} />
+        <Route path="/rates" element={<RatesPage />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Route>
+    </Routes>
   );
 };
