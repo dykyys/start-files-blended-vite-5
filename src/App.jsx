fc96655819ad82bd6c8e852aae31d3f402052300
Oleksandr Lutsenko <sashalutsenko1991@gmail.com>
2024-04-06 10:34:38 +0200

    task1.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
-import { Heading } from 'components';
+import { Route, Routes, Navigate } from "react-router-dom";
+import { Header } from "components";
+import { lazy } from "react";
+
+
+const Home = lazy(() => import('pages/Home'));
+const Rates = lazy(() => import('pages/Rates'));
 
 export const App = () => {
-  return <Heading title="qwerty" />;
+  return (
+    <Routes>
+      <Route path="/" element={<Header />}>
+        <Route index element={<Home />}/>
+        <Route path="/rates" element={<Rates />}/>
+        <Route path="*" element={ <Navigate to="/" replace/>} />
+        </Route> 
+    </Routes>
+  )
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import { BrowserRouter } from 'react-router-dom';
 import { App } from './App.jsx';
 
 import 'modern-normalize/modern-normalize.css';
@@ -8,6 +8,8 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+   <BrowserRouter>
+   <App />
+   </BrowserRouter>
   </React.StrictMode>,
 );
