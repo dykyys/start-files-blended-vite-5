df19ea5016ee036236d763c79b56823bb147d177
Elena Zhuvak <elenkazh@gmail.com>
2025-02-09 09:09:09 +0000

    adds routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,42 @@
-import Heading from './components/Heading/Heading';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Home from './pages/Home';
+import Rates from './pages/Rates';
+import { useEffect } from 'react';
+import { getUserInfo } from './service/opencagedataApi';
 
 export const App = () => {
-  return <Heading title="Just do it!" />;
+  useEffect(() => {
+    const options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
+    };
+    
+    async function success(pos) {
+      const crd = pos.coords;
+      console.log('crd', crd)
+
+      const data = await getUserInfo(crd)
+      console.log(data.results[0].annotations.currency.iso_code)
+
+    }
+    
+    function error(err) {
+      console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+    
+    window.navigator.geolocation.getCurrentPosition(success, error, options);
+    
+  }, [])
+  
+  return (
+    <Routes>
+      <Route  path="/" element={<Header />}>
+        <Route index element={<Home />} />
+        <Route path="/rates" element={<Rates />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Route>
+    </Routes>
+  );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,12 @@ import { App } from './App.jsx';
 
 import 'modern-normalize/modern-normalize.css';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter future={{v7_relativeSplatPath: true, v7_startTransition: true}}>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
